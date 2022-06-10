66d4755d85b4e64a20b6d970f5816e3abc3bd419
max3kkk <fedorovm093@gmail.com>
2022-06-10 19:34:02 +0300

    feat: add OrderAdmin(status and phone filters) general table

--- shop/django_app/admin.py
+++ shop/django_app/admin.py
@@ -9,10 +9,30 @@ class CustomerAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description', )
-    list_filter = ('id', )
-    search_fields = ('title', )
+    list_display = ('id', 'title', 'description',)
+    list_filter = ('id',)
+    search_fields = ('title',)
 
 
-admin.site.register(Order)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_cust_name', 'get_cust_phone', 'get_cust_email', 'address', 'status')
+    list_filter = ('status', 'customer_id__phone_number')
+
+    def get_cust_name(self, obj):
+        return obj.customer_id.name
+    get_cust_name.short_description = 'Customer Name'
+    get_cust_name.admin_order_name = 'customer_id__name'
+
+    def get_cust_phone(self, obj):
+        return obj.customer_id.phone_number
+    get_cust_phone.short_description = 'Customer Phone Number'
+    get_cust_name.admin_order_name = 'customer_id__phone_number'
+
+    def get_cust_email(self, obj):
+        return obj.customer_id.email
+    get_cust_email.short_description = 'Customer Email'
+    get_cust_name.admin_order_name = 'customer_id__email'
+
+
 admin.site.register(OrderItem)
--- shop/django_app/models.py
+++ shop/django_app/models.py
@@ -32,11 +32,12 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.title + ': ' + self.description
+        return f'{self.title}: {self.description}'
 
 
 class OrderItem(models.Model):
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
