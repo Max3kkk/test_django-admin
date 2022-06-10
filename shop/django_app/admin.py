8ba97a96c12535d173ca8ee46642b316613e26a7
max3kkk <fedorovm093@gmail.com>
2022-06-10 18:29:33 +0300

    feat: add CustomerAdmin

--- shop/django_app/admin.py
+++ shop/django_app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Customer, Order, OrderItem, Product
 
-admin.site.register(Customer)
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'email')
+
+
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Product)
