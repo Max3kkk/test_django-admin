fc474c5470c3630cf616695e6757bbbf5474f763
max3kkk <fedorovm093@gmail.com>
2022-06-10 18:43:27 +0300

    feat: add ProductAdmin with filters

--- shop/django_app/admin.py
+++ shop/django_app/admin.py
@@ -4,9 +4,15 @@ from .models import Customer, Order, OrderItem, Product
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone_number', 'email')
+    list_display = ('id', 'name', 'phone_number', 'email')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', )
+    list_filter = ('id', )
+    search_fields = ('title', )
 
 
 admin.site.register(Order)
 admin.site.register(OrderItem)
-admin.site.register(Product)
--- shop/django_app/models.py
+++ shop/django_app/models.py
@@ -29,9 +29,10 @@ class Order(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.title
+        return self.title + ': ' + self.description
 
 
 class OrderItem(models.Model):
