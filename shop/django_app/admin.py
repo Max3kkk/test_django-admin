3975aca4062755edc1600887ac4caa1fbf786eac
max3kkk <fedorovm093@gmail.com>
2022-06-17 23:24:00 +0300

    refactor: use get_queryset, add InputFilter/placeholder, filter file

--- shop/django_app/admin.py
+++ shop/django_app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.db.models import Sum
 from django.http import HttpResponseRedirect
 
+from .admin_filters import IdFilter, OrderSumFilter
 from .models import Customer, Order, OrderItem, Product
 
 
@@ -21,37 +22,14 @@ class OrderItemInline(admin.TabularInline):
 class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
     list_display = ('id', 'title', 'description',)
-    list_filter = ('id',)
+    list_filter = (IdFilter,)
     search_fields = ('title',)
-
-
-class OrderSumFilter(admin.SimpleListFilter):
-    title = 'Order Total sum'
-    parameter_name = 'order_total_cost'
-
-    def lookups(self, request, model_admin):
-        return (
-            ('<100', 'less than 100rub'),
-            ('<500', 'less than 500rub'),
-            ('<1000', 'less than 10000rub'),
-            ('<10000', 'less than 10000rub'),
-        )
-
-    def queryset(self, request, queryset):
-        value = self.value()
-        if value == '<100':
-            return Order.objects.annotate(total_cost=Sum('item_prices__price')).filter(total_cost__lte=100)
-        elif value == '<500':
-            return Order.objects.annotate(total_cost=Sum('item_prices__price')).filter(total_cost__lte=500)
-        elif value == '<1000':
-            return Order.objects.annotate(total_cost=Sum('item_prices__price')).filter(total_cost__lte=1000)
-        elif value == '<10000':
-            return Order.objects.annotate(total_cost=Sum('item_prices__price')).filter(total_cost__lte=10000)
-        return queryset
+    search_help_text = 'Input product title'
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    list_select_related = ('customer',)
     actions = ['cancel_orders']
     inlines = [OrderItemInline, ]
     list_display = (
@@ -63,6 +41,13 @@ class OrderAdmin(admin.ModelAdmin):
     fields = (
         'id', 'customer', 'customer_name', 'customer_phone', 'customer_email', 'address', 'created_at', 'status',
         'order_cost')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            _order_cost=Sum('item_prices__price'),
+        )
+        return queryset
 
     @admin.display(description='Cust. Name')
     def customer_name(self, obj):
@@ -78,11 +63,11 @@ class OrderAdmin(admin.ModelAdmin):
 
     @admin.display(description='Items')
     def item_list(self, obj):
-        return list(OrderItem.objects.filter(order=obj.id))
+        return list(OrderItem.objects.filter(order_id=obj.id))
 
     @admin.display(description='Total Cost')
     def order_cost(self, obj):
-        return sum(list(OrderItem.objects.filter(order=obj.id).values_list('price', flat=True)))
+        return obj._order_cost
 
     change_form_template = "admin/cancel_order.html"
 
